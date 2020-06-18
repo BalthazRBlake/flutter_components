be7deb941336208f811b27092a540858dc32ec2b
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-18 22:52:35 +0300

    refactor cargarData() to be async-await for proper loading of opciones on app init

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,4 +1,6 @@
 
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle ;
 
 class _MenuProvider {
@@ -6,16 +8,18 @@ class _MenuProvider {
   List<dynamic> opciones = [];
 
   _MenuProvider() {
-    cargarData();
+    //cargarData();
   }
 
-  cargarData() {
+  Future<List<dynamic>> cargarData() async {
 
-    rootBundle.loadString('data/menu_opts.json')
-      .then((data) { 
-        print(data);
-      });
+    final response = await rootBundle.loadString('data/menu_opts.json');
+        
+    Map dataMap = json.decode(response);
+    print(dataMap['rutas']);
+    opciones = dataMap['rutas'];
 
+    return opciones;
   }
 }
 
