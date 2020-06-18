700720acdbe40033ac02c215d16bf40210f3274b
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-18 14:00:32 +0300

    ListView with ListTile and Divider

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -3,8 +3,25 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Components Temp'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile( // common Widget to fill a ListView
+            title: Text('ListTile Title')
+          ),
+          Divider(),
+          ListTile( // common Widget to fill a ListView
+            title: Text('ListTile Title')
+          ),
+          Divider(),
+          ListTile( // common Widget to fill a ListView
+            title: Text('ListTile Title')
+          ),
+        ],
+      ),
     );
   }
 }
