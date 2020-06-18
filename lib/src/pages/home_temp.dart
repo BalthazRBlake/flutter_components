8b57e196eeee7edef0bda523d07070bd60e3b015
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-18 15:53:56 +0300

    return a List<Widget> for the List view

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis', 'Siete'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,20 +11,19 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Components Temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile( // common Widget to fill a ListView
-            title: Text('ListTile Title')
-          ),
-          Divider(),
-          ListTile( // common Widget to fill a ListView
-            title: Text('ListTile Title')
-          ),
-          Divider(),
-          ListTile( // common Widget to fill a ListView
-            title: Text('ListTile Title')
-          ),
-        ],
+        children: _getItems(),
       ),
     );
+  }
+
+  List<Widget> _getItems() {
+    final items = new List<Widget>();
+
+    for(String opcion in opciones) {
+      items.add(Text('$opcion'));
+      items.add(Divider());
+    }
+
+    return items;
   }
 }
